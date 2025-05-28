c6a76fd3bb35940012ee4aa28c475ebe6cfaa931
djoudj-dev <nedellec.julien.78@gmail.com>
2025-05-28 17:46:07 +0200

    feat(config(build)): ajoute un filtre global et le support des fichiers statiques
    - Implémente `AllExceptionsFilter` comme filtre d'exceptions global pour une gestion cohérente des erreurs.
    - Configure la gestion des fichiers statiques pour le répertoire "uploads" avec un préfixe `/uploads/`.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AllExceptionsFilter } from './common/exceptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Apply global exception filter
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  // Serve static files from the uploads directory
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   // Enable CORS for Angular frontend
   let allowedOrigins = ['http://localhost:4200']; // Default fallback
